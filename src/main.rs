65266f102a803bd69ff4fe4d0d3c026d2b616ad3
Jonny Ingall <jonathanaingall@gmail.com>
2024-11-02 01:12:34 +0000

    Add event handler and logging to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,132 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    fs::OpenOptions,
+    io::Write,
+    time::{Instant, SystemTime},
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Event {
+    id: String,
+    /// UTC Timestamp of the event
+    ts: u64,
+    event: StateEvent,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+enum StateEvent {
+    /// Adds a new ship to the oceans
+    EnrolShip { ship: String },
+
+    /// The ship has arrived at a port
+    Arrival { ship: String, port: Port },
+
+    /// The ship has left a port and is out at sea
+    Departure { ship: String },
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+enum Port {
+    SanFrancisco,
+    Porto,
+    LosAngeles,
+    HongKong,
+    Tokyo,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Ship {
+    name: String,
+    port: Option<Port>,
+}
+
+impl Ship {
+    fn new(name: String, port: Option<Port>) -> Ship {
+        Ship { name, port }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct State {
+    ships: HashMap<String, Ship>,
+}
+
+fn process_event(event: StateEvent, state: &mut State) {
+    log_event(Event {
+        id: "a".into(),
+        ts: SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("UTC timestamp should be after UNIX_EPOCH")
+            .as_secs(),
+        event: event.clone(),
+    });
+    match event {
+        StateEvent::EnrolShip { ship } => {
+            state.ships.insert(ship.clone(), Ship::new(ship, None));
+        }
+        StateEvent::Arrival { ship, port } => {
+            state
+                .ships
+                .entry(ship)
+                .and_modify(|s| s.port = Some(port.clone()));
+        }
+        StateEvent::Departure { ship } => {
+            state.ships.entry(ship).and_modify(|s| s.port = None);
+        }
+    };
+    println!("new state: {:?}", state);
+}
+
+fn log_event(event: Event) {
+    println!("Writing event: {:?}", event);
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("log.jsonl")
+        .unwrap();
+    let as_json = serde_json::to_string(&event).expect("event should have been serialized as JSON");
+
+    writeln!(file, "{}", as_json).expect("event should have been logged");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut state = State {
+        ships: HashMap::default(),
+    };
+
+    process_event(
+        StateEvent::EnrolShip {
+            ship: "hms_at_sea".into(),
+        },
+        &mut state,
+    );
+    process_event(
+        StateEvent::EnrolShip {
+            ship: "hms_hello".into(),
+        },
+        &mut state,
+    );
+    process_event(
+        StateEvent::Arrival {
+            ship: "hms_at_sea".into(),
+            port: Port::SanFrancisco,
+        },
+        &mut state,
+    );
+    process_event(
+        StateEvent::Arrival {
+            ship: "hms_hello".into(),
+            port: Port::Tokyo,
+        },
+        &mut state,
+    );
+    process_event(
+        StateEvent::Departure {
+            ship: "hms_hello".into(),
+        },
+        &mut state,
+    );
 }
